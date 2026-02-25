e963aa85d082dd6a9323d4e0d534e5ca3784c9f6
justin-nr <justin98025@gmail.com>
2026-02-25 15:55:18 -0800

    Node and Edge class created-ish

--- src/DijkstrasComponents/Edge.java
+++ src/DijkstrasComponents/Edge.java
@@ -5,16 +5,18 @@ public class Edge {
     //Variables
     String label;
     String description;
+    String direction;
     int weight;
 
     //creates object
-    Object edge(String label, String description, int weight) {
+    Object edge(String label, String description, String direction, int weight) {
         //applies varibles to object
         this.label = label;
         this.weight = weight;
+        this.direction = direction;
         this.description = description;
 
         //returns the applied variables with object
-        return edge(label, description, weight); //null as placeholder
+        return edge(label, description, direction, weight); //null as placeholder
     }
 }
