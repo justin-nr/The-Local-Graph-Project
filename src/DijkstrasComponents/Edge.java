9d93ad5b3b45f2481c7c3ff0ddc3971eef6878dd
justin-nr <justin98025@gmail.com>
2026-03-09 13:18:51 -0700

    Algorithm has arrived. doesnt work rn

--- src/DijkstrasComponents/Edge.java
+++ src/DijkstrasComponents/Edge.java
@@ -3,20 +3,18 @@ package DijkstrasComponents;
 public class Edge {
 
     //Variables
-    String label;
     String description;
-    String direction;
+    int destination;
+    int direction;
     int weight;
 
     //creates object
-    Object edge(String label, String description, String direction, int weight) {
+    public Edge(String description,int destination, int direction, int weight) {
         //applies varibles to object
-        this.label = label;
         this.weight = weight;
+        this.destination = destination;
         this.direction = direction;
         this.description = description;
 
-        //returns the applied variables with object
-        return edge(label, description, direction, weight); //null as placeholder
     }
 }
